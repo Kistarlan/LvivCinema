e5bd5670c40a38b2e1d4f2b0a7d903fd62b98610
Oleg Rozkvas <oleg.rozkvas@gmail.com>
2016-12-07 17:41:04 +0200

    Change DisplayInfo in Actors

--- LvivCinema/Models/Actor.cs
+++ LvivCinema/Models/Actor.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace LvivCinema.Models
 {
     public class Actor
     {
+        [ScaffoldColumn(false)]
         public int Id  { set; get;}
+
+        [Display(Name="Ім'я")]
         public string Name { set; get;}
+        [Display(Name = "Прізвище")]
         public string Surname { set; get;}
+        [Display(Name = "Рік народження")]
         public int Year  { set; get;}
+
+        [Display(Name = "Фільми")]
         public virtual ICollection<Film> Films { set; get; }
 
         public Actor()
